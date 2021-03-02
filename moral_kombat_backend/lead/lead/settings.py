56d18f90d366d486eb3365a889f0392e937ec531
Ishaan Khurana <khuranaishaan5@gmail.com>
2021-03-02 11:01:09 -0500

    KB-13 03/02/2021 Updated the settings.py and docker-compose files such that the database configuration values are pulled in from the env file instead of relying on the hardcoded values

--- moral_kombat_backend/lead/lead/settings.py
+++ moral_kombat_backend/lead/lead/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from corsheaders.defaults import default_headers
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -94,18 +95,18 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
 
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cnehcbso',
-        'USER': 'cnehcbso',
-        'PASSWORD': 'qy2xdb_zEcAZFOmY7fvQT1SddHRUhbCI',
-        'HOST': 'raja.db.elephantsql.com',
-        'PORT': '5432',
         # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': 'db',
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'postgres',
-        # 'HOST': 'postgres-db',
-        # 'PORT': '5432'
+        # 'NAME': 'cnehcbso',
+        # 'USER': 'cnehcbso',
+        # 'PASSWORD': 'qy2xdb_zEcAZFOmY7fvQT1SddHRUhbCI',
+        # 'HOST': 'raja.db.elephantsql.com',
+        # 'PORT': '5432',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['POSTGRESDB'],
+        'USER': os.environ['POSTGRESUSERNAME'],
+        'PASSWORD': os.environ['POSTGRESPASSWORD'],
+        'HOST': os.environ['POSTGRESHOST'],
+        'PORT': '5432'
     }
 }
 
