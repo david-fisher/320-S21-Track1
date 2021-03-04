50b6fb45fcf60061f1805a1e34e62ab2d35c8cce
thaild912 <thaild912@gmail.com>
2021-03-04 17:40:24 +0700

    Preliminary code for API demo

--- steminist_simulator_backend/simulator_backend/new/admin.py
+++ steminist_simulator_backend/simulator_backend/new/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from new.models import *
+
+
+admin.site.register(Scenario)
+admin.site.register(Course)
+admin.site.register(PartOf)
+admin.site.register(User)
+admin.site.register(Enrolled)
+admin.site.register(Page)
