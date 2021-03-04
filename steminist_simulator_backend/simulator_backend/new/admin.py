7e7bf1bd46165705be3800e37a1e35a4a176fb98
Jit Gan <jitli98@live.com>
2021-03-04 18:24:14 +0800

    Made the entities modifiable in DjangoAdmin

--- steminist_simulator_backend/simulator_backend/new/admin.py
+++ steminist_simulator_backend/simulator_backend/new/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Scenario, Course, PartOf, User, Enrolled, Page
+
+admin.site.register(Scenario)
+admin.site.register(Course)
+admin.site.register(PartOf)
+admin.site.register(User)
+admin.site.register(Enrolled)
+admin.site.register(Page)
+
+
