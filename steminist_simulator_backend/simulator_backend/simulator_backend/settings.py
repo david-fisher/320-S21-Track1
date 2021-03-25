5112f36c76634393b7d20b66b66ca7518e3822fa
Jit Gan <jitli98@live.com>
2021-03-25 14:55:09 +0800

    Import new schemas changes

--- steminist_simulator_backend/simulator_backend/simulator_backend/settings.py
+++ steminist_simulator_backend/simulator_backend/simulator_backend/settings.py
@@ -32,14 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'backend',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'backend'
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
