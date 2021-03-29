4c766240e22e499556e56e9e81a519f1b2205476
Jit Gan <jitli98@live.com>
2021-03-30 02:32:05 +0800

    Integrated first 3 APIs with new Django models

--- steminist_simulator_backend/simulator_backend/backend/views.py
+++ steminist_simulator_backend/simulator_backend/backend/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from django.core.serializers import serialize
 from django.http import HttpResponse, JsonResponse, HttpResponseForbidden, HttpResponseNotFound, HttpResponseBadRequest
 import backend.models as md
+from rest_framework import status
+from rest_framework.response import Response
 import json
 import logging
 
+from django.db import connection
 
 INTROPAGE = 1
 TASKPAGE = 2
@@ -28,65 +31,80 @@ def index(request):
 
 def scenarios(request):
     jsonData = json.loads(request.body)
-    studentID = jsonData['studentId']
+    userId = jsonData['userId']
 
-    if not isinstance(studentID, int):
-        print("Invalid student ID")
-        return HttpResponseBadRequest('Invalid student ID: %s' %str(studentID))
+    if not isinstance(userId, int):
+        print("Invalid user ID")
+        return JsonResponse(status=400, data={'status': 400, 'message': 'Invalid User ID: ' + str(userId)})
     else:
         try:
-            scenarioQuerySet = md.Scenario.objects.filter(id=studentID)
-            # If no scenarios with the given studentId were found, return 404 error
-            if len(scenarioQuerySet) == 0:
-                return HttpResponseNotFound('Scenario not found with the given studentId')
+            versionIdQuerySet = md.AssignedTo.objects.filter(user_id=userId).values_list('version_id')
+            scenarioVersionQuerySet = md.Version.objects.filter(version_id__in=versionIdQuerySet)\
+                                        .values('version_id', 'name', 'num_conversation', 'first_page')
 
-            resultData = (list(scenarioQuerySet.values()))   
+            # If no scenarios with the given userId were found, return 404 error
+            if len(scenarioVersionQuerySet) == 0:
+                return JsonResponse(status=404, data={'status': 404, 'message': 'Scenario not found with the given User ID'}, )
+
+            resultData = list(scenarioVersionQuerySet)
+
         except Exception as ex:
             logging.exception("Exception thrown: Query Failed to retrieve Scenario")
 
         print("Got all scenarios")
-        return JsonResponse({'status':200, 'result': resultData}, content_type="application/json")
+        return JsonResponse(status=200, data={'status': 200, 'message':'success', 'result': resultData})
 
 def scenarioIntroduction(request):
     jsonData = json.loads(request.body)
-    scenarioID = jsonData['scenarioId']
+    versionID = jsonData['versionId']
+    pageID = jsonData['pageId']
 
-    if not isinstance(scenarioID, int):
-        print("Invalid ID")
-        return HttpResponseBadRequest('Invalid scenario ID: %s' %str(scenarioID))
+    if not isinstance(versionID, int):
+        print("Invalid version ID")
+        return JsonResponse(status=400, data={'status': 400, 'message': 'Invalid Version ID: ' + str(versionID)})
+    elif not isinstance(pageID, int):
+        print("Invalid page ID")
+        return JsonResponse(status=400, data={'status': 400, 'message': 'Invalid page ID: ' + str(pageID)})
     else:
         try:
-            scenarioIntroQuerySet = md.Page.objects.filter(order=INTROPAGE, scenario_id=scenarioID)
-            # If no pages with the given scenarioId and order were found, return 404 error
+            scenarioIntroQuerySet = md.Page.objects.filter(page_id=pageID, version_id=versionID)\
+                                      .values("page_id", "page_type", "page_title", "version_id", "body", "next_page")
+            # If no pages with the given versionId and order were found, return 404 error
             if len(scenarioIntroQuerySet) == 0:
-                return HttpResponseNotFound('Page not found with the given scenarioId')
+                return JsonResponse(status=404, data={'status': 404,
+                                                      'message': 'Page not found with the given Version ID'})
 
-            resultData = (list(scenarioIntroQuerySet.values()))
+            resultData = list(scenarioIntroQuerySet)
         except Exception as ex:
             logging.exception("Exception thrown: Query Failed to retrieve Page")
 
         print("Got scenario introduction.")
-        return JsonResponse({'status':200, 'result': resultData}, content_type="application/json")
-
+        return JsonResponse(status=200, data={'status': 200, 'message': 'success', 'result': resultData})
 
 def scenarioTask(request):
     jsonData = json.loads(request.body)
-    scenarioID = jsonData['scenarioId']
+    versionID = jsonData['versionId']
+    pageID = jsonData['pageId']
 
-    if not isinstance(scenarioID, int):
-        print("Invalid ID")
-        return HttpResponseBadRequest('Invalid scenario ID: %s' % str(scenarioID))
+    if not isinstance(versionID, int):
+        print("Invalid Version ID")
+        return JsonResponse(status=400, data={'status': 400, 'message': 'Invalid Version ID'})
+    elif not isinstance(pageID, int):
+        print("Invalid page ID")
+        return JsonResponse(status=400, data={'status': 400, 'message': 'Invalid page ID'})
     else:
         try:
-            scenarioTaskQuerySet = md.Page.objects.filter(order=TASKPAGE, scenario_id=scenarioID)
-            # If no pages with the given scenarioId and order were found, return 404 error
+            scenarioTaskQuerySet = md.Page.objects.filter(page_id=pageID, version_id=versionID) \
+                .values("page_id", "page_type", "page_title", "version_id", "body", "next_page")
+            # If no pages with the given versionId and pageId are found, return 404 error
             if len(scenarioTaskQuerySet) == 0:
-                return HttpResponseNotFound('Page not found with the given scenarioId')
+                return JsonResponse(status=404, data={'status': 404,
+                                                      'message': '‘No task page found base on given Version ID’'})
 
-            resultData = (list(scenarioTaskQuerySet.values()))
+            resultData = list(scenarioTaskQuerySet)
         except Exception as ex:
             logging.exception("Exception thrown: Query Failed to retrieve Page")
 
-        print("Got scenario introduction.")
-        return JsonResponse({'status': 200, 'result': resultData}, content_type="application/json")
+        print("Got scenario task.")
+        return JsonResponse(status=200, data={'status': 200, 'message': 'success', 'result': resultData})
 
