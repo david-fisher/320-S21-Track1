3177c32e5b812f51f9b2a47f515a8b5b66abc124
Radhika Junnare <radhika.junnare@gmail.com>
2021-04-14 14:43:21 -0400

    Update urls.py

--- simulator/steminist_simulator_backend/simulator_backend/backend/urls.py
+++ simulator/steminist_simulator_backend/simulator_backend/backend/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', views.getDisplayName, name='getDisplayName')
+    path('', views.getPrimaryAffiliation, name='getPrimaryAffiliation')
+    path('', views.getMail, name='getMail')
+    path('', views.checkAttribute, name='checkAttribute')
     path('scenarios', views.scenarios, name='scenarios'),
     path('scenarios/intro', views.scenarioIntroduction, name='scenarioIntroduction'),
     path('scenarios/task', views.scenarioTask, name='scenarioTask'),
