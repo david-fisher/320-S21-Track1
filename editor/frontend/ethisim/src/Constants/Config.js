8ac7645009d5cef8c4b77fd0f9151200e9228c10
Jack Williams <44446909+jackwilliams2480@users.noreply.github.com>
2021-04-26 09:32:22 -0400

    Protect db
    Using shib env to protect database

--- editor/frontend/ethisim/src/Constants/Config.js
+++ editor/frontend/ethisim/src/Constants/Config.js
@@ -1,2 +1,2 @@
 export const DOMAIN = (process.env.NODE_ENV === 'production') ? process.env.REACT_APP_URL : 'http://localhost';
-export const baseURL = DOMAIN + ':8000';
+export const baseURL = DOMAIN + ':8001';
