91b444d97c82d6c67bf5b37945ad48a8eaa38cbb
Ishaan Khurana <khuranaishaan5@gmail.com>
2021-05-30 17:42:17 -0400

    SMETH-4 05/30/21 fixed a few typos

--- .github/scripts/server.sh
+++ .github/scripts/server.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-cd /ethisim
+cd /shared/docker
 docker-compose down
-docker pull ikhurana/ethisim-dev:frontend-landing-page 
-docker pull ikhurana/ethisim-dev:frontend-editor
-docker pull ikhurana/ethisim-dev:frontend-simulator
-docker pull ikhurana/ethisim-dev:backend-editor
-docker pull ikhurana/ethisim-dev:backend-simulator
+docker pull ikhurana/ethisim-prod:frontend-landing-page 
+docker pull ikhurana/ethisim-prod:frontend-editor
+docker pull ikhurana/ethisim-prod:frontend-simulator
+docker pull ikhurana/ethisim-prod:backend-editor
+docker pull ikhurana/ethisim-prod:backend-simulator
 docker rmi docker_production:latest
 docker-compose up -d
 yes | docker system prune -a --filter "until=168h"
