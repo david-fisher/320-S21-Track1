ee9a4e7636b07da56ed135e7dcdd9d8cd131f2dd
Ishaan Khurana <khuranaishaan5@gmail.com>
2021-06-21 22:47:38 -0400

    06/21 Added a new virtualhost block for the admin panel

--- editor/backend/lead/lead/settings.py
+++ editor/backend/lead/lead/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',  
     'django.middleware.common.CommonMiddleware', 
+    'lead.middleware.AuthMiddleware'
 ]
 CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
