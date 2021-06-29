a951ca2c6b2e01ef40cb7bc4560221340458b173
Ishaan Khurana <khuranaishaan5@gmail.com>
2021-06-28 23:51:54 -0400

    06/28/21 just a test to see if the build runs successfully or not

--- simulator/frontend/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-npm install --silent
-npm install react-scripts@3.4.1 -g --silent
-
-# # add app
-# COPY . ./
-
-REACT_APP_URL=https://ethisim1.cs.umass.edu  REACT_APP_APIKEY=${API_TOKEN} npm run build
